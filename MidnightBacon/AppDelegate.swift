80b3afa9b317b7c36f5a1c553d58ff31c22c7c6b
Justin Kolb <franticapparatus@gmail.com>
2014-10-24 07:38:49 -0500

    Only attach to window after it is created.

--- MidnightBacon/AppDelegate.swift
+++ MidnightBacon/AppDelegate.swift
@@ -16,11 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor(red: 51.0/255.0, green: 102.0/255.0, blue: 153.0/255.0, alpha: 1.0)]
 
-        storyboard = ApplicationStoryboard()
-        storyboard.attachToWindow(window)
-
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window.tintColor = UIColor(red: 51.0/255.0, green: 102.0/255.0, blue: 153.0/255.0, alpha: 1.0)
+
+        storyboard = ApplicationStoryboard()
+        storyboard.attachToWindow(window)
         window.makeKeyAndVisible()
         
         return true
