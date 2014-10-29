fbfcc452b63edf4268bb6fe5529979a3ccf2ee21
jkolb <franticapparatus@gmail.com>
2014-10-29 16:59:46 -0500

    Add loading indicator to web view.

--- MidnightBacon/WebViewController.swift
+++ MidnightBacon/WebViewController.swift
@@ -11,15 +11,34 @@ import WebKit
 
 class WebViewController : UIViewController {
     var webView: WKWebView!
+    var activityIndicator: UIActivityIndicatorView!
     var url: NSURL!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         webView = WKWebView()
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .New, context: nil)
         view.addSubview(webView)
         
+        activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .WhiteLarge)
+        activityIndicator.color = GlobalStyle().redditOrangeRedColor
+        activityIndicator.startAnimating()
+        view.addSubview(activityIndicator)
+        
         webView.loadRequest(NSURLRequest(URL: url))
+    }
+    
+    deinit {
+        webView.removeObserver(self, forKeyPath: "estimatedProgress")
+    }
+    
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        if keyPath == "estimatedProgress" {
+            if webView.estimatedProgress >= 1.0 {
+                activityIndicator.stopAnimating()
+            }
+        }
     }
     
     override func viewWillLayoutSubviews() {
@@ -28,6 +47,11 @@ class WebViewController : UIViewController {
             Top(equalTo: view.bounds.top),
             Width(equalTo: view.bounds.width),
             Height(equalTo: view.bounds.height)
+        )
+        
+        activityIndicator.frame = activityIndicator.layout(
+            CenterX(equalTo: view.bounds.centerX),
+            CenterY(equalTo: view.bounds.centerY)
         )
     }
 }
