e40c5ef4765b4ef8d710fa6f7813a4e947bc5a46
jkolb <franticapparatus@gmail.com>
2014-11-20 08:25:06 -0600

    Call other initializer instead of having duplicate logic.

--- MidnightBacon/Utility/UIColor+RGBA.swift
+++ MidnightBacon/Utility/UIColor+RGBA.swift
@@ -17,9 +17,9 @@ extension UIColor {
     }
     
     convenience init(_ rgb: UInt32) {
-        let r = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
-        let g = CGFloat((rgb & 0x0000FF00) >> 08) / 255.0
-        let b = CGFloat((rgb & 0x000000FF) >> 00) / 255.0
-        self.init(red: r, green: g, blue: b, alpha: 1.0)
+        let r = UInt8((rgb & 0x00FF0000) >> 16)
+        let g = UInt8((rgb & 0x0000FF00) >> 08)
+        let b = UInt8((rgb & 0x000000FF) >> 00)
+        self.init(r, g, b)
     }
 }
