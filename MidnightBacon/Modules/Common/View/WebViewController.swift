9b9e0c61ae10cce066a372a4dbc387e90cf3ccad
jkolb <franticapparatus@gmail.com>
2014-12-31 12:54:17 -0600

    Log out navigation delegates to see if I can tell why sometimes I get a white screen and no content loaded. Stop using KVO to show loading indicator and use navigation delegate instead.

--- MidnightBacon/Modules/Common/View/WebViewController.swift
+++ MidnightBacon/Modules/Common/View/WebViewController.swift
@@ -10,37 +10,25 @@ import UIKit
 import WebKit
 import DrapierLayout
 
-class WebViewController : UIViewController {
+class WebViewController : UIViewController, WKNavigationDelegate {
     var style: Style!
     var webView: WKWebView!
     var activityIndicator: UIActivityIndicatorView!
     var url: NSURL!
+    var currentNavigation: WKNavigation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         webView = WKWebView()
-        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .New, context: nil)
+        webView.navigationDelegate = self
         view.addSubview(webView)
         
         activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .WhiteLarge)
         activityIndicator.color = style.redditOrangeRedColor
-        activityIndicator.startAnimating()
         view.addSubview(activityIndicator)
         
-        webView.loadRequest(NSURLRequest(URL: url))
-    }
-    
-    deinit {
-        webView.removeObserver(self, forKeyPath: "estimatedProgress")
-    }
-    
-    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
-        if keyPath == "estimatedProgress" {
-            if webView.estimatedProgress >= 1.0 {
-                activityIndicator.stopAnimating()
-            }
-        }
+        currentNavigation = webView.loadRequest(NSURLRequest(URL: url))
     }
     
     override func viewWillLayoutSubviews() {
@@ -55,5 +43,64 @@ class WebViewController : UIViewController {
             CenterX(equalTo: view.bounds.centerX),
             CenterY(equalTo: view.bounds.centerY)
         )
+    }
+    
+    // MARK: - WKNavigationDelegate
+    
+    func webView(webView: WKWebView, decidePolicyForNavigationAction navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
+        NSLog("decidePolicyForNavigationAction: %@ %@", navigationAction.navigationType.stringValue, navigationAction)
+        decisionHandler(.Allow)
+    }
+    
+    func webView(webView: WKWebView, decidePolicyForNavigationResponse navigationResponse: WKNavigationResponse, decisionHandler: (WKNavigationResponsePolicy) -> Void) {
+        NSLog("decidePolicyForNavigationResponse: %@", navigationResponse)
+        decisionHandler(.Allow)
+    }
+    
+    func webView(webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        NSLog("didStartProvisionalNavigation: %@", navigation)
+        activityIndicator.startAnimating()
+    }
+    
+    func webView(webView: WKWebView, didReceiveServerRedirectForProvisionalNavigation navigation: WKNavigation!) {
+        NSLog("didReceiveServerRedirectForProvisionalNavigation: %@", navigation)
+    }
+    
+    func webView(webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: NSError) {
+        NSLog("didFailProvisionalNavigation:withError: %@ %@", navigation, error)
+        activityIndicator.stopAnimating()
+    }
+    
+    func webView(webView: WKWebView, didCommitNavigation navigation: WKNavigation!) {
+        NSLog("didCommitNavigation: %@", navigation)
+    }
+    
+    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+        NSLog("didFinishNavigation: %@", navigation)
+        activityIndicator.stopAnimating()
+    }
+    
+    func webView(webView: WKWebView, didFailNavigation navigation: WKNavigation!, withError error: NSError) {
+        NSLog("didFailNavigation:withError: %@ %@", navigation, error)
+        activityIndicator.stopAnimating()
+    }
+}
+
+extension WKNavigationType {
+    var stringValue: String {
+        switch self {
+        case .BackForward:
+            return "BackForward"
+        case .FormResubmitted:
+            return "FormResubmitted"
+        case .FormSubmitted:
+            return "FormSubmitted"
+        case .LinkActivated:
+            return "LinkActivated"
+        case .Other:
+            return "Other"
+        case .Reload:
+            return "Reload"
+        }
     }
 }
